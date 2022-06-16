cb18bee9645341ac5b82794f848897594e84acfd
Kachii Cheong <Kach93@gmail.com>
2022-06-16 19:37:21 +0100

    initial commit

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const settings = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const database = "database";
+
+mongoose.connect(`mongodb://localhost:27017/${database}`, settings);
+
+module.exports = mongoose;
